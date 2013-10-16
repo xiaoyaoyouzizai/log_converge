659a0ab47c91776601a30b850fa11f6cb612eca1
yan.cong <yan.cong@renren-inc.com>
2013-10-16 16:15:45 +0800

    add main program

---
+++ LocalScribeHandler.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+require 'scribe'
+
+class LocalScribeHandler < LogfileTransfer::Handler
+  def init
+    @scribe_client = Scribe.new('127.0.0.1:1463')
+  end
+  def handle log_path, log_fn, line, line_count, pattern
+    @scribe_client.log(line, pattern)
+  end
+end
+
+LogfileTransfer.run ARGV, 2001, File.expand_path(File.dirname(__FILE__))
